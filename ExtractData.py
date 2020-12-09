cf0c37d90b2db32544a71505de5e5ab269e2d70d
Octave Victor <victoroctave@gmail.com>
2020-12-09 14:26:24 +0100

    extract data from database

---
+++ ExtractData.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+sqliteConnection = False
+
+try:
+    sqliteConnection = sqlite3.connect('C:\\Users\\Victor\\Documents\\Cours\\I3\\S9\\IA\\Projet\\database.sqlite')
+    cursor = sqliteConnection.cursor()
+    print("Database created and Successfully Connected to SQLite")
+
+    homeTeamId = cursor.execute("select home_team_api_id from Match where id=1489;").fetchall()[0][0]
+    awayTeamId = cursor.execute("select away_team_api_id from Match where id=1489;").fetchall()[0][0]
+    dateMatch = cursor.execute("select date from Match where id=1489;").fetchall()[0][0]
+
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
