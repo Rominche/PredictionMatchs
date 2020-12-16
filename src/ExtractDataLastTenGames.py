fc8fc457af5490b28150eef05289dded82eaff48
Octave Victor <victoroctave@gmail.com>
2020-12-16 15:56:53 +0100

    Fix issue when no previous game

--- src/ExtractDataLastTenGames.py
+++ src/ExtractDataLastTenGames.py
@@ -15,30 +15,34 @@ def last_ten_games(match_id, cursor):
                                         + ") and date<'" + gameDate + "' order by date asc").fetchall()
     if len(previousGamesDates) > 10:
         lastTenGamesDates = previousGamesDates[-10:][0][0]
-    elif len(previousGamesDates) == 0:
-        return 0, 0, 0, 0, 0, 0
-    else:
+    elif len(previousGamesDates) > 0:
         lastTenGamesDates = previousGamesDates[0][0]
 
-    # Get last 10 results
-    homeTeamDraw = cursor.execute("select count(*) from Match where (home_team_api_id=" + str(homeTeamId)
-                                  + " or away_team_api_id=" + str(homeTeamId)
-                                  + ") and date<'" + gameDate + "' and date>='" + lastTenGamesDates
-                                  + "' and home_team_goal=away_team_goal").fetchall()[0][0]
-    homeTeamWin = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(homeTeamId)
-                                 + " and home_team_goal>away_team_goal) or (away_team_api_id=" + str(homeTeamId)
-                                 + " and home_team_goal<away_team_goal)) and date<'" + gameDate + "' and date>='"
-                                 + lastTenGamesDates + "'").fetchall()[0][0]
-    homeTeamLoss = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(homeTeamId)
-                                  + " and home_team_goal<away_team_goal) or (away_team_api_id=" + str(homeTeamId)
-                                  + " and home_team_goal>away_team_goal)) and date<'" + gameDate + "' and date>='"
-                                  + lastTenGamesDates + "'").fetchall()[0][0]
+    if len(previousGamesDates) != 0:
+        # Get last 10 results
+        homeTeamDraw = cursor.execute("select count(*) from Match where (home_team_api_id=" + str(homeTeamId)
+                                      + " or away_team_api_id=" + str(homeTeamId)
+                                      + ") and date<'" + gameDate + "' and date>='" + lastTenGamesDates
+                                      + "' and home_team_goal=away_team_goal").fetchall()[0][0]
+        homeTeamWin = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(homeTeamId)
+                                     + " and home_team_goal>away_team_goal) or (away_team_api_id=" + str(homeTeamId)
+                                     + " and home_team_goal<away_team_goal)) and date<'" + gameDate + "' and date>='"
+                                     + lastTenGamesDates + "'").fetchall()[0][0]
+        homeTeamLoss = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(homeTeamId)
+                                      + " and home_team_goal<away_team_goal) or (away_team_api_id=" + str(homeTeamId)
+                                      + " and home_team_goal>away_team_goal)) and date<'" + gameDate + "' and date>='"
+                                      + lastTenGamesDates + "'").fetchall()[0][0]
 
-    # Form on last 10 games
-    nbGames = homeTeamDraw + homeTeamLoss + homeTeamWin
-    homeWinPercentage = '{:.2f}'.format(homeTeamWin / nbGames)
-    homeDrawPercentage = '{:.2f}'.format(homeTeamDraw / nbGames)
-    homeLossPercentage = '{:.2f}'.format(homeTeamLoss / nbGames)
+        # Form on last 10 games
+        nbGames = homeTeamDraw + homeTeamLoss + homeTeamWin
+        homeWinPercentage = '{:.2f}'.format(homeTeamWin / nbGames)
+        homeDrawPercentage = '{:.2f}'.format(homeTeamDraw / nbGames)
+        homeLossPercentage = '{:.2f}'.format(homeTeamLoss / nbGames)
+
+    else:
+        homeWinPercentage = 0.33
+        homeDrawPercentage = 0.33
+        homeLossPercentage = 0.33
 
     # print("Home team last " + str(nbGames) + " games :")
     # print(str(homeWinPercentage) + "% W, " + str(homeDrawPercentage) + "% D, " +
@@ -53,28 +57,34 @@ def last_ten_games(match_id, cursor):
 
     if len(previousGamesDates) > 10:
         lastTenGamesDates = previousGamesDates[-10:][0][0]
-    else:
+    elif len(previousGamesDates) > 0:
         lastTenGamesDates = previousGamesDates[0][0]
 
-    # Get last 10 results
-    awayTeamDraw = cursor.execute("select count(*) from Match where (home_team_api_id=" + str(awayTeamId)
-                                  + " or away_team_api_id=" + str(awayTeamId)
-                                  + ") and date<'" + gameDate + "' and date>='" + lastTenGamesDates
-                                  + "' and home_team_goal=away_team_goal").fetchall()[0][0]
-    awayTeamLose = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(awayTeamId)
-                                  + " and home_team_goal<away_team_goal) or (away_team_api_id=" + str(awayTeamId)
-                                  + " and home_team_goal>away_team_goal)) and date<'" + gameDate + "' and date>='"
-                                  + lastTenGamesDates + "'").fetchall()[0][0]
-    awayTeamVictory = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(awayTeamId)
-                                     + " and home_team_goal>away_team_goal) or (away_team_api_id=" + str(awayTeamId)
-                                     + " and home_team_goal<away_team_goal)) and date<'" + gameDate
-                                     + "' and date>='" + lastTenGamesDates + "'").fetchall()[0][0]
+    if len(previousGamesDates) != 0:
+        # Get last 10 results
+        awayTeamDraw = cursor.execute("select count(*) from Match where (home_team_api_id=" + str(awayTeamId)
+                                      + " or away_team_api_id=" + str(awayTeamId)
+                                      + ") and date<'" + gameDate + "' and date>='" + lastTenGamesDates
+                                      + "' and home_team_goal=away_team_goal").fetchall()[0][0]
+        awayTeamLose = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(awayTeamId)
+                                      + " and home_team_goal<away_team_goal) or (away_team_api_id=" + str(awayTeamId)
+                                      + " and home_team_goal>away_team_goal)) and date<'" + gameDate + "' and date>='"
+                                      + lastTenGamesDates + "'").fetchall()[0][0]
+        awayTeamVictory = cursor.execute("select count(*) from Match where ((home_team_api_id=" + str(awayTeamId)
+                                         + " and home_team_goal>away_team_goal) or (away_team_api_id=" + str(awayTeamId)
+                                         + " and home_team_goal<away_team_goal)) and date<'" + gameDate
+                                         + "' and date>='" + lastTenGamesDates + "'").fetchall()[0][0]
 
-    # Form on last 10 games
-    nbGames = awayTeamDraw + awayTeamLose + awayTeamVictory
-    awayWinPercentage = '{:.2f}'.format(awayTeamVictory / nbGames)
-    awayDrawPercentage = '{:.2f}'.format(awayTeamDraw / nbGames)
-    awayLossPercentage = '{:.2f}'.format(awayTeamLose / nbGames)
+        # Form on last 10 games
+        nbGames = awayTeamDraw + awayTeamLose + awayTeamVictory
+        awayWinPercentage = '{:.2f}'.format(awayTeamVictory / nbGames)
+        awayDrawPercentage = '{:.2f}'.format(awayTeamDraw / nbGames)
+        awayLossPercentage = '{:.2f}'.format(awayTeamLose / nbGames)
+
+    else:
+        awayWinPercentage = 0.33
+        awayDrawPercentage = 0.33
+        awayLossPercentage = 0.33
 
     # print("Away team last " + str(nbGames) + " games :")
     # print(str(awayWinPercentage) + "% W, " + str(awayDrawPercentage) +
