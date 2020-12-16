4ea875b4b8e8885c6506d9ea274b7f41c144c9b9
vignonem <emelyne.vignon@reseau.eseo.fr>
2020-12-16 13:59:05 +0100

    [player attributes] function

--- src/ExtractData_PlayerAttributes.py
+++ src/ExtractData_PlayerAttributes.py
@@ -1,21 +1,22 @@
 import sqlite3
 import configparser
 
-sqliteConnection = False
 
-config = configparser.ConfigParser()
-config.read('../config/config.ini')
-path = config.get('DEFAULT', 'PATH')
+def team_overall(id_match):
+    config = configparser.ConfigParser()
+    config.read('../config/config.ini')
+    path = config.get('DEFAULT', 'PATH')
 
-try:
-    sqliteConnection = sqlite3.connect(path)
-    cursor = sqliteConnection.cursor()
-    print("Database created and Successfully Connected to SQLite")
+    sqlite_connection = False
+    home_overall = 0
+    away_overall = 0
 
+    try:
+        sqlite_connection = sqlite3.connect(path)
+        cursor = sqlite_connection.cursor()
+        print("Database created and Successfully Connected to SQLite")
 
-    def team_overall(id):
-
-        match_id = "from Match where id=" + str(id)
+        match_id = "from Match where id=" + str(id_match)
         date = cursor.execute("select date " + match_id).fetchall()[0][0]
 
         list_home_player = cursor.execute("select home_player_1, home_player_2, home_player_3,"
@@ -28,7 +29,6 @@ try:
                                           " away_player_8, away_player_9, away_player_10, away_player_11 "
                                           + match_id).fetchall()[0]
 
-        home_overall = 0
         for i in list_home_player:
             player_overall = cursor.execute("select overall_rating "
                                             "from Player_Attributes "
@@ -39,7 +39,6 @@ try:
 
         home_overall /= len(list_home_player)
 
-        away_overall = 0
         for i in list_away_player:
             player_overall = cursor.execute("select overall_rating "
                                             "from Player_Attributes "
@@ -50,14 +49,13 @@ try:
 
         away_overall /= len(list_away_player)
 
-        print(home_overall)
-        print(away_overall)
-
         cursor.close()
 
-except sqlite3.Error as error:
-    print("Error while connecting to sqlite", error)
-finally:
-    if sqliteConnection:
-        sqliteConnection.close()
-        print("The SQLite connection is closed")
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("The SQLite connection is closed")
+
+    return home_overall, away_overall
