33756b5c26d3040e796acfd7c3f718ede71f6a5a
Octave Victor <victoroctave@gmail.com>
2020-12-20 19:33:36 +0100

    Update predict to use new csv file

--- src/AnalyseData/predict.py
+++ src/AnalyseData/predict.py
@@ -1,26 +1,33 @@
 import numpy as np
 import pandas as pd
+np.random.seed(7)
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 
 donnees_data_frame = pd.read_csv ('../db/data2015_2016.csv' , delimiter=" ")
 donnees_ensemble_total= donnees_data_frame.values
 
+donnees_data_frame2 = pd.read_csv ('../db/data2012_2015.csv' , delimiter=" ")
+donnees_ensemble_total2= donnees_data_frame2.values
+
 nombre_lignes_base=donnees_ensemble_total.shape[0]
 nombre_colonnes_base=donnees_ensemble_total.shape[1]
 
-x_train = donnees_ensemble_total[0:round(nombre_lignes_base*3/4),1:donnees_ensemble_total.shape[1]-4]
-y_train = donnees_ensemble_total[0:round(nombre_lignes_base*3/4),donnees_ensemble_total.shape[1]-1:]
+nombre_lignes_base2=donnees_ensemble_total2.shape[0]
+nombre_colonnes_base2=donnees_ensemble_total2.shape[1]
 
-x_test = donnees_ensemble_total[round(nombre_lignes_base*3/4)+1:,1:donnees_ensemble_total.shape[1]-4]
-y_test = donnees_ensemble_total[round(nombre_lignes_base*3/4)+1:,donnees_ensemble_total.shape[1]-1:]
+x_train = donnees_ensemble_total2[0:nombre_lignes_base2,1:nombre_colonnes_base2-4]
+y_train = donnees_ensemble_total2[0:nombre_lignes_base2,nombre_colonnes_base2-1:]
 
-nbr_neurones = 2
+x_test = donnees_ensemble_total[0:nombre_lignes_base,1:nombre_colonnes_base-4]
+y_test = donnees_ensemble_total[0:nombre_lignes_base,nombre_colonnes_base-1:]
+
+nbr_neurones = 8
 
 nbr_iterations = 20
 
 i = 0
-while (i < 10):
+while i < 10:
     model = MLPClassifier(hidden_layer_sizes=[nbr_neurones], random_state=7, max_iter=nbr_iterations)
     model.fit(x_train, y_train)
 
@@ -37,7 +44,7 @@ plt.plot(y_pred_test, "r:o", label="prédite")
 plt.legend()
 
 plt.title("Ensemble TEST - Nombre neurones = " + str(nbr_neurones))
-
+plt.get_current_fig_manager().window.state('zoomed')
 plt.show()
 
 #evaluation : taux d'erreur
